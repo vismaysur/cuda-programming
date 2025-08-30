b06916f724a3975a2de6a944bc63649ed650ae86
Vismay Suramwar <vismaysuramwar@gmail.com>
2025-08-30 21:15:44 +0000

    feat: complete vector addition kernel

--- kernels/vector_add.cu
+++ kernels/vector_add.cu
@@ -1,29 +1,108 @@
 #include <cuda_runtime.h>
-#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define N 10000000
+#define BLOCK_SIZE 256
+
+void initRand(float *vec, int n) {
+    for (int i = 0; i < n; i++) {
+        vec[i] = rand();
+    }
+}
+
+void vector_add_cpu(float* a, float* b, float* c, int n) {
+    for (int i = 0; i < n; i++) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+__global__ void vector_add_gpu(float* a, float* b, float* c, int n) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (i < n) 
+        c[i] = a[i] + b[i];
+}
+
+double getTime() {
+    struct timespec ts;
+
+    if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
+        perror("clock_gettime()");
+        return -1;
+    }
+
+    return ts.tv_sec + pow(ts.tv_nsec, -9);
+}
 
 int main() {
-   float* h_a, *h_b, *h_c_cpu, *h_c_gpu;
-   float* d_a, *d_b, *d_c;
+    float* h_a, *h_b, *h_c_cpu, *h_c_gpu;
+    float* d_a, *d_b, *d_c;
 
-   size_t size = N * sizeof(float);
+    size_t size = N * sizeof(float);
 
-   h_a = malloc(size);
-   h_b = malloc(size);
-   h_c = malloc(size);
+    h_a = (float*) malloc(size);
+    h_b = (float*) malloc(size);
+    h_c_cpu = (float*) malloc(size);
+    h_c_gpu = (float*) malloc(size);
 
-   initRand(h_a, N);
-   initRand(h_b, N);
-  
-   cudaMalloc(&d_a, size);
-   cudaMalloc(&d_b, size);
-   cudaMalloc(&d_c, size);
+    srand(time(0));
 
-   free(h_a);
-   free(h_b);
-   free(h_c);
-   free(d_a);
-   free(d_b);
-   free(d_c);
+    initRand(h_a, N);
+    initRand(h_b, N);
+
+    cudaMalloc(&d_a, size);
+    cudaMalloc(&d_b, size);
+    cudaMalloc(&d_c, size);
+
+    cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice);
+
+    int numBlocks = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    printf("Performing CPU benchmarking...\n");
+    double cpu_time = 0.0;
+    for (int i = 0; i < 20; i++) {
+        double start = getTime();
+        vector_add_cpu(h_a, h_b, h_c_cpu, N);
+        double end = getTime();
+        cpu_time += end - start;
+    }
+    cpu_time /= 20;
+
+    printf("Performing GPU benchmarking...\n");
+    double gpu_time = 0.0;
+    for (int i = 0; i < 20; i++) {
+        double start = getTime();
+        vector_add_gpu<<<numBlocks, BLOCK_SIZE>>>(d_a, d_b, d_c, N);
+        double end = getTime();
+        gpu_time += end - start;
+    }
+    gpu_time /= 20;
+
+    printf("CPU execution time: %f milliseconds\n", cpu_time*1000);
+    printf("GPU execution time: %f milliseconds\n", gpu_time*1000);
+    printf("GPU speed up over CPU: %fx\n", cpu_time / gpu_time);
+
+    cudaMemcpy(h_c_gpu, d_c, size, cudaMemcpyDeviceToHost);
+    bool correct = true;
+    for (int i = 0; i < N; i++) {
+        if (h_c_gpu[i] != h_c_cpu[i]) {
+            correct = false;
+            break;
+        }
+    }
+
+    printf("Results are %s\n", correct ? "correct" : "incorrect");
+
+    free(h_a);
+    free(h_b);
+    free(h_c_cpu);
+    free(h_c_gpu);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    return 0;
 }
